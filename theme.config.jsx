8f105aa5447b8af54907f5201cc8d165b359ad4f
AyeMate <ayemate@quack.host>
2023-03-10 02:35:06 +0100

    1

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    logo: (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <img src="/quackhost.webp" alt="logo" width={40} height={40} />
+      <span style={{ fontSize: 24, fontWeight: 700, marginLeft: 10 }}>QuackVault</span>
+    </div>
+    ),
+    docsRepositoryBase: 'https://github.com/quack-host/quack-vault',
+    footer: {
+        text: <span>{new Date().getFullYear()} © QuackHost. Built by quacks and geese in high places ❤️.</span>
+    },
+}
